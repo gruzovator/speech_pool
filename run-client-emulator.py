6b7fbe5e3a001e2ea9ae4623ec0b80f82e48d6d8
gruzovator <gruzovator@gmail.com>
2017-11-21 22:47:15 +0300

    add parallelism to client

--- run-client-emulator.py
+++ run-client-emulator.py
@@ -10,6 +10,7 @@ import logging
 import socket
 import threading
 import time
+from multiprocessing.dummy import Pool  # threads pool
 
 import jsonrpcclient
 
@@ -18,11 +19,12 @@ logging.basicConfig(level=logging.ERROR,
                     datefmt='%Y-%m-%dT%H:%M:%S')
 logging.getLogger('jsonrpcclient.client.request').setLevel(logging.ERROR)
 logging.getLogger('jsonrpcclient.client.response').setLevel(logging.ERROR)
-log = logging.getLogger('speech_pool_client')
-log.setLevel(logging.DEBUG)
+logger = logging.getLogger('speech_pool_client')
+logger.setLevel(logging.DEBUG)
 
 
-def call_stop_cmd(delay, api_url, request_id):
+def call_stop_cmd(client_id, delay, api_url, request_id):
+    log = logger.getChild('%d' % client_id)
     try:
         time.sleep(delay)
         log.info('calling <stop_speek> command')
@@ -45,32 +47,39 @@ def main():
     parser.add_argument('-t', '--text', required=True, help='text to convert to audio')
     parser.add_argument('-d', '--start-stop-delay', default=5, type=int,
                         help='delay between start_play and stop_play commands, seconds')
+    parser.add_argument('-n', default=1, type=int, help='run multiple clients')
     args = parser.parse_args()
 
-    # simple tcp socket server to handle incoming data
+    def run_client(client_id):
+        log = logger.getChild('%d' % client_id)
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+            server_socket.bind(('127.0.0.1', 0))  # random port
+            server_socket.listen(1)
+            server_address = server_socket.getsockname()
+            log.info('waiting data on %s', server_address)
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-        server_socket.bind(('127.0.0.1', 0))  # random port
-        server_socket.listen(1)
-        server_address = server_socket.getsockname()
-        log.info('waiting data on %s', server_address)
+            log.info('calling <start_speek>')
+            request_id = jsonrpcclient.request(args.api_url, 'start_speek', args.text,
+                                               server_address[0], server_address[1], 'my notification')
+            stop_thread = threading.Thread(target=call_stop_cmd,
+                                           args=(client_id, args.start_stop_delay, args.api_url, request_id))
+            stop_thread.daemon = True
+            stop_thread.start()
 
-        log.info('calling <start_speek>')
-        request_id = jsonrpcclient.request(args.api_url, 'start_speek', args.text,
-                                           server_address[0], server_address[1], 'my notification')
-        stop_thread = threading.Thread(target=call_stop_cmd, args=(args.start_stop_delay, args.api_url, request_id))
-        stop_thread.daemon = True
-        stop_thread.start()
+            conn, _ = server_socket.accept()
+            log.info('incoming connection')
+            with conn:
+                while True:
+                    data = conn.recv(4096)
+                    if not data:
+                        break
+                    log.info('<DATA>: %s', data)
+            log.info('incoming connection end')
 
-        conn, _ = server_socket.accept()
-        log.info('incoming connection')
-        with conn:
-            while True:
-                data = conn.recv(4096)
-                if not data:
-                    break
-                log.info('<DATA>: %s', data)
-        log.info('incoming connection end')
+    pool = Pool()
+    pool.map(run_client, range(args.n))
+    pool.close()
+    pool.join()
 
 
 if __name__ == '__main__':
